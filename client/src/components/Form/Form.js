b39b3acd49b5a2bd593f97d17d9e07df0fcf9f91
ethadnguyen <tringuyen1382003@gmail.com>
2022-06-13 15:27:15 +0700

    second commit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import Filebase from "react-file-base64";
 import { useDispatch } from "react-redux";
-import { createPosts } from "../../actions/posts";
-const Form = () => {
+import { createPosts, updatePost } from "../../actions/posts";
+import { useSelector } from "react-redux";
+// GET THE CURRENT ID
+
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: "",
     title: "",
@@ -12,16 +15,38 @@ const Form = () => {
     tags: "",
     selectedFile: "",
   });
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((message) => message._id === currentId) : null
+  );
   const classes = useStyles();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(createPosts(postData));
+    if (currentId === 0) {
+      dispatch(createPosts(postData));
+    } else {
+      dispatch(updatePost(currentId, postData));
+    }
+    clear();
   };
 
-  const clear = () => {};
+  const clear = () => {
+    setCurrentId(null);
+
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
 
   return (
     <div>
@@ -32,7 +57,9 @@ const Form = () => {
           className={`${classes.root} ${classes.form}`}
           onSubmit={handleSubmit}
         >
-          <Typography variant="h6">Creating a memory</Typography>
+          <Typography variant="h6">
+            {currentId ? "Editing" : "Creating"} a memory
+          </Typography>
           <TextField
             name="creator"
             variant="outlined"
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Post from "./Post/Post";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 import useStyles from "./styles";
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
-  console.log(posts);
-  return (
-    <div>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </div>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid item key={post._id} xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 export default Posts;
