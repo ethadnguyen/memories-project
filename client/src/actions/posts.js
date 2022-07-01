471152280035f62f11a5a0025697bf9101b8fd2f
Ethad <tringuyen1382003@gmail.com>
2022-07-01 19:42:03 +0700

    third commit

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,20 +1,26 @@
 import * as api from "../api";
-
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  LIKE,
+  DELETE,
+} from "../constants/actionTypes";
 // Action Creators
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
-    dispatch({ type: "FETCH_ALL", payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error.message);
   }
 };
 
-export const createPosts = (post) => async (dispatch) => {
+export const createPost = (post) => async (dispatch) => {
   try {
-    const { data } = await api.createPosts(post);
+    const { data } = await api.createPost(post);
 
-    dispatch({ type: "CREATE", payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -23,7 +29,27 @@ export const createPosts = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error);
   }
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-import Filebase from "react-file-base64";
-import { useDispatch } from "react-redux";
-import { createPosts, updatePost } from "../../actions/posts";
-import { useSelector } from "react-redux";
-// GET THE CURRENT ID
+import { useDispatch, useSelector } from "react-redux";
+import FileBase from "react-file-base64";
+import useStyles from "./styles";
+import { createPost, updatePost } from "../../actions/posts";
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
@@ -18,27 +16,15 @@ const Form = ({ currentId, setCurrentId }) => {
   const post = useSelector((state) =>
     currentId ? state.posts.find((message) => message._id === currentId) : null
   );
-  const classes = useStyles();
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     if (post) setPostData(post);
   }, [post]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (currentId === 0) {
-      dispatch(createPosts(postData));
-    } else {
-      dispatch(updatePost(currentId, postData));
-    }
-    clear();
-  };
-
   const clear = () => {
-    setCurrentId(null);
-
+    setCurrentId(0);
     setPostData({
       creator: "",
       title: "",
@@ -48,88 +34,100 @@ const Form = ({ currentId, setCurrentId }) => {
     });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (currentId === 0) {
+      dispatch(createPost(postData));
+      clear();
+    } else {
+      dispatch(updatePost(currentId, postData));
+      clear();
+    }
+  };
+
   return (
-    <div>
-      <Paper className={classes.paper}>
-        <form
-          autoComplete="off"
-          noValidate
-          className={`${classes.root} ${classes.form}`}
-          onSubmit={handleSubmit}
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">
+          {currentId ? `Editing "${post.title}"` : "Creating a Memory"}
+        </Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          multiline
+          rows={4}
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags (coma separated)"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData({ ...postData, tags: e.target.value.split(",") })
+          }
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
         >
-          <Typography variant="h6">
-            {currentId ? "Editing" : "Creating"} a memory
-          </Typography>
-          <TextField
-            name="creator"
-            variant="outlined"
-            label="Creator"
-            fullWidth
-            value={postData.creator}
-            onChange={(e) =>
-              setPostData({ ...postData, creator: e.target.value })
-            }
-          />
-          <TextField
-            name="title"
-            variant="outlined"
-            label="Title"
-            fullWidth
-            value={postData.title}
-            onChange={(e) =>
-              setPostData({ ...postData, title: e.target.value })
-            }
-          />
-          <TextField
-            name="message"
-            variant="outlined"
-            label="Message"
-            fullWidth
-            value={postData.message}
-            onChange={(e) =>
-              setPostData({ ...postData, message: e.target.value })
-            }
-          />
-          <TextField
-            name="tags"
-            variant="outlined"
-            label="Tags"
-            fullWidth
-            value={postData.tags}
-            onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
-          />
-          <div className={classes.fileInput}>
-            <Filebase
-              type="file"
-              mutiple={false}
-              onDone={({ base64 }) =>
-                setPostData({ ...postData, selectedFile: base64 })
-              }
-            />
-          </div>
-          <Button
-            className={classes.buttonSubmit}
-            variant="contained"
-            color="primary"
-            size="large"
-            type="submit"
-            fullWidth
-            // onClick={handleSubmit}
-          >
-            Submit
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            size="small"
-            onClick={clear}
-            fullWidth
-          >
-            Clear
-          </Button>
-        </form>
-      </Paper>
-    </div>
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
   );
 };
+
 export default Form;
